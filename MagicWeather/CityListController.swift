49f773e17cf11aa33b403cbc72f2a13a08d28cbc
alatan <althygmgl@gmail.com>
2015-11-12 21:42:55 +0800

    提交工程

---
+++ MagicWeather/CityListController.swift
@@ -0,0 +1,116 @@
+//
+//  CityListController.swift
+//  MagicWeather
+//
+//  Created by maiziedu on 11/12/15.
+//  Copyright © 2015 Alatan. All rights reserved.
+//
+
+import UIKit
+
+class CityListController: UITableViewController {
+
+    var citysArr: NSArray?
+    var cityWearIds: NSArray?
+    
+    // 闭包传值
+    var selectedCity: ((cityName: String) ->(Void))?
+    var selectedCityWearID: ((cityWID: String) -> (Void))?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.citysArr = []
+        
+        // 添加返回按钮
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "返回", style: UIBarButtonItemStyle.Plain, target: self, action: "dismiss")
+        
+        //  获取城市列表
+        self.getCityList()
+    }
+    
+    func dismiss() {
+        
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return (self.citysArr?.count)!
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        cell.textLabel?.text = self.citysArr![indexPath.row]["citynm"] as! String
+
+        return cell
+    }
+    
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cityDic: NSDictionary = self.citysArr![indexPath.row] as! NSDictionary
+        selectedCity!(cityName: cityDic["citynm"] as! String)
+        
+        selectedCityWearID!(cityWID: self.cityWearIds![indexPath.row] as! String)
+        
+        
+        self.dismiss()
+    }
+    
+    
+    func getCityList() {
+        
+        let url: NSURL = NSURL(string: "http://api.k780.com:88/?app=weather.city&cou=1&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json")!
+        
+        
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            
+            if error != nil {
+                
+                NSLog("请求出错！")
+            } else {
+                
+//                let responseString = NSString.init(data: data!, encoding: NSUTF8StringEncoding)
+                
+                do {
+                    
+                    // json 数据转字典
+                    let jsonData = (try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)) as! NSDictionary
+                    
+                    // 筛选出 result 字典
+                    let result = jsonData["result"] as! NSDictionary
+                    
+                    // 获取所有地名
+                    let citys: NSArray = result.allValues  // 字典都是 key-value
+                    
+                    self.citysArr = citys
+                    self.cityWearIds = result.allKeys as NSArray
+                    self.tableView.reloadData()
+                } catch {
+                    
+                }
+            }
+        }
+        
+    }
+
+
+}
--- MagicWeather/ViewController.swift
+++ MagicWeather/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 城市名
+    @IBOutlet weak var cityNameLabel: UILabel!
+    // 天气
+    @IBOutlet weak var weatherLabel: UILabel!
+    // 气温
+    @IBOutlet weak var TempLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.getCityTodayWeatherByName("1")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,89 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 城市当天的天气
+    func getCityTodayWeatherByName(cityName: String) {
+    
+        let url: NSURL = NSURL(string: "http://api.k780.com:88/?app=weather.today&weaid=\(cityName)&&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json")!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            
+            if error != nil {
+                
+                NSLog("请求出错！")
+            } else {
+                
+//                let responseString = NSString.init(data: data!, encoding: NSUTF8StringEncoding)
+                
+                do {
+                    
+                    let jsonData = (try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)) as! NSDictionary
+                    
+                    self.cityNameLabel.text = jsonData["result"]!["citynm"] as? String
+                    self.weatherLabel.text = jsonData["result"]!["weather"] as? String
+                    self.TempLabel.text = jsonData["result"]!["temp_curr"] as? String
+                    self.TempLabel.text?.appendContentsOf("℃")
+                } catch {
+                    
+                }
+            }
+        }
+    }
 
+    
+    // 城市未来一周的天气
+    func getCitySevenDaysWeather(cityName: String) {
+        
+        // http://api.k780.com:88/?app=weather.today&weaid=1&&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json
+        
+        let url: NSURL = NSURL(string: "http://api.k780.com:88/?app=weather.future&weaid=1&&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json")!
+
+        
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            
+            if error != nil {
+                
+                NSLog("请求出错！")
+            } else {
+                
+                let responseString = NSString.init(data: data!, encoding: NSUTF8StringEncoding)
+                
+                do {
+                    
+                    let jsonData = (try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)) as! NSDictionary
+                    print("\(jsonData)")
+                } catch {
+                    
+                }
+            }
+        }
+
+    }
+    
+    @IBAction func presenCityContoller(sender: UIButton) {
+        
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("Nav") as! UINavigationController
+        
+        let cityVC = vc.topViewController as! CityListController
+        cityVC.selectedCity = { (cityName: String) in
+            
+            print("\(cityName)")
+//            self.getCityTodayWeatherByName(cityName)
+        }
+        
+        cityVC.selectedCityWearID = { (cityWearID: String) in
+            
+            print("\(cityWearID)")
+            self.getCityTodayWeatherByName("\(cityWearID)")
+        }
+        
+        self.presentViewController(vc, animated: true) { () -> Void in
+            
+        }
+    }
+    
 }
 
